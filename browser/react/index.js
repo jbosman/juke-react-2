fa7b65bd8d332dfb9b58d1ba97551049769dddff
Joe Bosman <joseph.a.bosman@gmail.com>
2017-05-08 17:50:25 -0400

    Starting point for react tutorial.

---
+++ browser/react/index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render( <h1>Hello World!</h1>, document.getElementById('app'));
+
+console.log('Hello React');
