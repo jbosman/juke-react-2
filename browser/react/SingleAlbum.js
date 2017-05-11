88a450c98f085cfd9625bbcc6147a538753c0445
Joe Bosman <joseph.a.bosman@gmail.com>
2017-05-11 19:28:07 -0400

    Removed {Component} destructoring from react library in functinoal components.

--- browser/react/SingleAlbum.js
+++ browser/react/SingleAlbum.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 function renderArtists(artists){
 	let artistsNames = artists.map( (artistInfo) => { return artistInfo.name });
