21c142b03fba598b67ec924954fc927e91e8aa21
Joe Bosman <joseph.a.bosman@gmail.com>
2017-05-11 19:00:06 -0400

    Refactored Sidebar to be a functional component.

--- browser/react/Sidebar.js
+++ browser/react/Sidebar.js
@@ -1,16 +1,17 @@
-import React, {Component} from 'react';
+import React from 'react';
 
-export default class Sidebar extends Component{
-	render(){
-		return (
-			<sidebar>
-			  <img src="juke.svg" className="logo" />
-			  <section>
-			    <h4 className="menu-item active">
-			      <a onClick={() => this.props.handleAlbumButtonClick() }>ALBUMS</a>
-			    </h4>
-			  </section>
-			</sidebar>
-		)
-	}
+export default function Sidebar(props){
+	const { handleAlbumButtonClick } = props;
+	
+	return (
+		<sidebar>
+		  <img src="juke.svg" className="logo" />
+		  <section>
+		    <h4 className="menu-item active">
+		      <a onClick={() => handleAlbumButtonClick() }>ALBUMS</a>
+		    </h4>
+		  </section>
+		</sidebar>
+	)
+	
 }
