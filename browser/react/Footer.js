330ac81894ed6bc575943cdb477e39ae22ba5ba4
Joe Bosman <joseph.a.bosman@gmail.com>
2017-05-11 18:57:12 -0400

    Refactored Footer to be functional component.

--- browser/react/Footer.js
+++ browser/react/Footer.js
@@ -1,47 +1,52 @@
 import React, {Component} from 'react';
 
-export default class Footer extends Component{
-	render(){
-		if(!this.props.currentSong.songId) return <div></div>;
-		return (
-			<footer>
-		        <div className="pull-left">
-		          <button 
-		          	onClick={ () => { this.props.handlePreviousButtonClick() } }
-		          	className="btn btn-default"
-		          >
-		            <span className="glyphicon glyphicon-step-backward"></span>
-		          </button>
-		          <button 
-		          	onClick={() => {this.props.handlePlayButtonClick(this.props.currentSong.songId)} }
-		          	className="btn btn-default">
-		            <span className= { 
-	 			      	this.props.currentSong.isPlaying ?
-	 			      	"glyphicon glyphicon-pause":
-	 			      	"glyphicon glyphicon-play"
-	 			      }
-	 			    >
-			     	</span>
-		          </button>
-		          <button 
-		          	onClick={ () => this.props.handleNextButtonClick() }
-		          	className="btn btn-default">
-		            <span className="glyphicon glyphicon-step-forward"></span>
-		          </button>
-		        </div>
-		        <div className="bar">
-		          <div className="progress">
-		            <div 
-		            	className="progress-bar" 
-		            	style={{
-		            		width: `${this.props.currentSong.progress}`,
-		            		backgroundColor: '#00BC8C'
-		            	}}
-		            >
-		            </div>
-		          </div>
-		        </div>
-		    </footer>
-		)
-	}
+export default function Footer(props){
+	const { 
+		currentSong,  
+		handlePlayButtonClick,
+		handleNextButtonClick,
+		handlePreviousButtonClick
+	} = props;
+
+	if(!currentSong.songId) return <div></div>;
+	return (
+		<footer>
+	        <div className="pull-left">
+	          <button 
+	          	onClick={ () => { handlePreviousButtonClick() } }
+	          	className="btn btn-default"
+	          >
+	            <span className="glyphicon glyphicon-step-backward"></span>
+	          </button>
+	          <button 
+	          	onClick={() => { handlePlayButtonClick( currentSong.songId) } }
+	          	className="btn btn-default">
+	            <span className= { 
+ 			      	currentSong.isPlaying ?
+ 			      	"glyphicon glyphicon-pause":
+ 			      	"glyphicon glyphicon-play"
+ 			      }
+ 			    >
+		     	</span>
+	          </button>
+	          <button 
+	          	onClick={ () => handleNextButtonClick() }
+	          	className="btn btn-default">
+	            <span className="glyphicon glyphicon-step-forward"></span>
+	          </button>
+	        </div>
+	        <div className="bar">
+	          <div className="progress">
+	            <div 
+	            	className="progress-bar" 
+	            	style={{
+	            		width: `${ currentSong.progress }`,
+	            		backgroundColor: '#00BC8C'
+	            	}}
+	            >
+	            </div>
+	          </div>
+	        </div>
+	    </footer>
+	)
 }
