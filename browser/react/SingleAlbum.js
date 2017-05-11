c294b410e654dc94eef9c7ef686346d9017b9457
Joe Bosman <joseph.a.bosman@gmail.com>
2017-05-11 19:04:50 -0400

    Refactored SingleAlbum to functional component.

--- browser/react/SingleAlbum.js
+++ browser/react/SingleAlbum.js
@@ -1,60 +1,64 @@
 import React, {Component} from 'react';
 
-export default class SingleAlbum extends Component {
+function renderArtists(artists){
+	let artistsNames = artists.map( (artistInfo) => { return artistInfo.name });
 
-	renderArtists(artists){
-		let artistsNames = artists.map( (artistInfo) => { return artistInfo.name });
+	// Build artists string correctly for single and multiple artists
+	return artistsNames.length === 1 ? 
+		artistsNames[0] :
+		artistsNames.join(', ').slice(-2);
+}
 
-		// Build artists string correctly for single and multiple artists
-		return artistsNames.length === 1 ? 
-			artistsNames[0] :
-			artistsNames.join(', ').slice(-2);
-	}
+export default function SingleAlbum(props){
 
-	render(){
-		const album = this.props.album;
-		if(!album) return;
-		return (
-			<div>
-			  <div>
-			    <h3>{album.name}</h3>
-			    <img src={`api/albums/${album.id}/image`} className="img-thumbnail" />
-			  </div>
-			  <table className='table'>
-			    <thead>
-			      <tr>
-			        <th></th>
-			        <th>Name</th>
-			        <th>Artists</th>
-			        <th>Genre</th>
-			      </tr>
-			    </thead>
-			    <tbody>
-			     {
-			     	album.songs.map( (song) => {
-			     		return (
-			     			<tr key={song.id}>
-			     			  <td>
-			     			    <button onClick={ () => this.props.handlePlayButtonClick(song.id) } className="btn btn-default btn-xs">
-			     			      <span className= { 
-			     			      	song.id === this.props.currentSong.songId && this.props.currentSong.isPlaying ?
-			     			      	"glyphicon glyphicon-pause":
-			     			      	"glyphicon glyphicon-play"
-			     			      }
-			     			      >
-			     			      </span>
-			     			    </button>
-			     			  </td>
-			     			  <td>{song.name}</td>
-			     			  <td>{this.renderArtists(song.artists)}</td>
-			     			  <td>{song.genre}</td>
-			     			</tr>
-			     		)
-			     	})
-			     }
-			    </tbody>
-			  </table>
-			</div>
-		)
-	}
+	const { 
+		album,
+		currentSong,
+		handlePlayButtonClick
+	} = props;
+
+	if(!album) return;
+	return (
+		<div>
+		  <div>
+		    <h3>{album.name}</h3>
+		    <img src={`api/albums/${album.id}/image`} className="img-thumbnail" />
+		  </div>
+		  <table className='table'>
+		    <thead>
+		      <tr>
+		        <th></th>
+		        <th>Name</th>
+		        <th>Artists</th>
+		        <th>Genre</th>
+		      </tr>
+		    </thead>
+		    <tbody>
+		     {
+		     	album.songs.map( (song) => {
+		     		return (
+		     			<tr key={song.id}>
+		     			  <td>
+		     			    <button onClick={ () => handlePlayButtonClick(song.id) } className="btn btn-default btn-xs">
+		     			      <span className= { 
+		     			      	song.id === currentSong.songId && currentSong.isPlaying ?
+		     			      	"glyphicon glyphicon-pause":
+		     			      	"glyphicon glyphicon-play"
+		     			      }
+		     			      >
+		     			      </span>
+		     			    </button>
+		     			  </td>
+		     			  <td>{song.name}</td>
+		     			  <td>{renderArtists(song.artists)}</td>
+		     			  <td>{song.genre}</td>
+		     			</tr>
+		     		)
+		     	})
+		     }
+		    </tbody>
+		  </table>
+		</div>
+	)
+	
 }
