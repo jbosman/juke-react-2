268652c0dd9b7df6c830085ab359f1d540f79009
Joe Bosman <joseph.a.bosman@gmail.com>
2017-05-15 19:03:44 -0400

    Main app path setup.

--- browser/react/index.js
+++ browser/react/index.js
@@ -2,8 +2,12 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
 
 import App from './App';
 
-ReactDOM.render( <App />
+ReactDOM.render( 
+	<Router history={hashHistory} >
+		<Route path='/' component={App} />
+	</Router>
 , document.getElementById('app') );
